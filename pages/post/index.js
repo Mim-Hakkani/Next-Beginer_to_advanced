23805f979d67bf143b88434b3933d06ed198ea83
32-Mimhakkani <mim.cse32@gmail.com>
2022-03-02 12:41:26 +0600

    link method using router.push method

--- pages/post/index.js
+++ pages/post/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import { useRouter } from "next/router";
+
 
 const Post = () => {
+
+   const router =useRouter();
+   
+    const handlesinglpage =()=>{
+
+        router.push('/post/singlepost')
+    }
     return (
         <div>
             <h1>Ehana Marketing Posts : </h1>
+            <button onClick={handlesinglpage}>PostDetails</button>
         </div>
     );
 };
